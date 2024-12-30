e73a11345afe5ba2190848da04be014027ebd4b3
Ahmad Alshara <111713746+Heratics@users.noreply.github.com>
2024-12-30 16:41:53 +0300

    Update in lec cin.get explained with e.g.cpp

--- in lec cin.get explained with e.g.cpp
+++ in lec cin.get explained with e.g.cpp
@@ -1,14 +1,5 @@
-/******************************************************************************
+// cin.get means it pulls any character it sees including a space
 
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-
- // cin.get means it pulls any character it sees including a space
- 
 #include <iostream>
 
 using namespace std;
